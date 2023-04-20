d03704092d482a031d493083fb1ff104e2746a85
Gal Ben Shmuel <galbenshmuel@gamil.com>
2023-04-20 17:07:05 +0300

    new

---
+++ WebApplication1/DTO/DecryptUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.DTO
+{
+    public class DecryptUserDTO
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public string Patient_Id { get; set; }
+        public int issuperuser { get; set; }
+    }
+}
